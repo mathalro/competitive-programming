3b72d175a505d373a5769f3a461a0c1314874976
matheus300 <matheus.ar11@hotmail.com>
2017-03-07 15:07:06 -0300

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define inf 0x3f3f3f3f
+#define mod 1e9+7
+#define debug(var) cerr << #var << ':' << var << ' '
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(false);
+	return 0;
+}
